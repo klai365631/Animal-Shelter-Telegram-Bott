80af12449d253b016571a804da03b44168815071
AllaKulchitskaya <alla070@list.ru>
2023-05-11 18:38:25 +0300

    initial commit

---
+++ src/test/java/skypro/teamproject/animalsheltertelegrambot/AnimalShelterTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.teamproject.animalsheltertelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
